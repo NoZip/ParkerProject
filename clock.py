e5b4624fbbd01c552d606a1bdf7475cd26e4a18e
eolhing <eolhing@gmail.com>
2013-02-06 16:13:15 +0100

    Implements clock properly

--- clock.py
+++ clock.py
@@ -1,23 +1,72 @@
+# import time
+
+# class Clock(object):
+# 	"""To manage time"""
+
+# 	def __init__(self):
+# 		self.currentTime = 0
+# 		self.lastTime = time.clock()
+# 		self.timeBuffer = self.lastTime
+	
+# 	def start():
+# 		self.lastTime = time.clock()
+# 		pass
+
+# 	@property
+# 	def time():
+# 		computeTime()
+# 		return currentTime
+
+# 	def computeTime() :
+# 		currentTime = time.clock()
+# 		timeBuffer += currentTime-lastTime
+# 		lastTime = currentTime
+
 import time
 
 class Clock(object):
-	"""To manage time"""
+	""" Class to manage time """
 
-	def __init__(self):
-		self.currentTime = 0
+	lastTime = 0
+	factor = 1
+	elapsedTime = 0
+	run = True
+ 	
+	def __init__(self) :
+		self.reset()
+ 
+	def start(self) :
+		self.run = True
+ 
+	def stop(self) :
+		self.run = False
+		self.reset(self)
+ 
+	def reset(self) :
 		self.lastTime = time.clock()
-		self.timeBuffer = self.lastTime
-	
-	def start():
-		self.lastTime = time.clock()
-		pass
-
-	@property
-	def time():
-		computeTime()
-		return currentTime
-
-	def computeTime() :
+		self.elapsedTime = 0
+ 
+	def pause(self) :
+		self.run = False
+ 
+	def getFactor(self) :
+		return self.factor
+ 
+	def setFactor(self, factor) :
+		self.getTime()
+		self.factor = factor
+ 
+	def getTime(self) :
 		currentTime = time.clock()
-		timeBuffer += currentTime-lastTime
-		lastTime = currentTime
+		if self.run :
+			self.elapsedTime += (currentTime - self.lastTime) * self.factor
+		self.lastTime = currentTime
+		return self.elapsedTime
+
+if __name__ == "__main__" :
+	clock = Clock()
+	clock.setFactor(1.5)
+	while(True) :
+		print(clock.getTime())
+		if(clock.getTime() > 2) :
+			clock.reset()
