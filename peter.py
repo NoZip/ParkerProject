a0f7b6a07a580fb5c01024f168ebe4f3d9a1b716
Thomas Pizon <thomas.pizon@etu.u-bordeaux1.fr>
2013-02-13 16:02:21 +0100

    Add poses function for legs

--- peter.py
+++ peter.py
@@ -8,13 +8,13 @@ class Leg(object):
 
 	a1 = 	5.000
 	a2 = 	-4.000
-	b = 	6.500
-	c = 	9.500
+	b  = 	6.500
+	c  = 	9.500
 
 	references = (0, 0, 0)
 	
 	def __init__(self, head, joint, tip):
-		self.motors = [head, joint, tip]
+		self.motors = (head, joint, tip)
 	
 	def raw_pose(self):
 		"returns leg's pose and return the motors position raw values"
@@ -30,27 +30,28 @@ class Leg(object):
 
 		return pose
 
+	def apply_raw_pose(self, raw_pose):
+		"docstring"
+		for value, motor in zip(pose, self.motors):
+			motor.position = value
+
 	def calibration(self):
-		self.references = self.pose()
+		self.references = self.raw_pose()
 
 	def pose(self):
 		"returns leg's pose and return the motors position according to our calibration references"
 		return tuple(value - reference for value, reference in zip(self.raw_pose(), self.references))
+
+	def apply_pose(self, pose):
+		"docstring"
+		for value, motor, reference in zip(pose, self.motors, self.references):
+			motor.position = reference + value
 
 	def position(self):
 		alpha = radians(self.motors[0].position - self.references[0])
 		beta = radians(self.motors[1].position - self.references[1])
 		gamma = -radians(self.motors[2].position - self.references[2])
 
-		# xa = 0
-		# ya = 0
-		# za = 0
-
-		# x = xa + self.a1 * cos(alpha)
-		# y = ya + self.a2 * sin(alpha)
-		# z = za 
-
-		#return Vector3D(alpha, beta, gamma)
 		return Vector3D(
 			x = cos(alpha) * (self.a1 + self.b * cos(beta) + self.c * cos(beta + gamma)),
 			y = sin(alpha) * (self.a1 + self.b * cos(beta) + self.c * cos(beta + gamma)),
