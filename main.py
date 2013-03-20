260475c47a705aed5724c9f1e800f592ffbf53a0
eolhing <eolhing@gmail.com>
2013-03-20 17:30:03 +0100

    Added drawSquareMode in main.py

--- main.py
+++ main.py
@@ -7,22 +7,21 @@ from bot import *
 from pose import *
 #from clock import Clock
 from setup import *
+import json
 
 
 def main():
-
 	ctrl = initCtrl()
+	drawSquareMode(ctrl)
+
 	# setupHW = None;
 	# setupSW = None;
-
 	# # dyn.enable_vrep()
 	# ctrl = initCtrl()
 	# setupHW = botSetup(ctrl, False)
-
 	# dyn.enable_vrep()
 	# ctrl = initCtrl()
 	# setupSW = botSetup(ctrl, True)
-
 	# idsHW = setupHW[0]
 	# factorsHW = setupHW[1]
 	# referenceHW = setupHW[2]
@@ -31,45 +30,56 @@ def main():
 	# referenceSW = setupSW[2]
 	# for i in range(len(idsHW)) :
 	# 	print i ," : ", referenceHW[idsHW[i]] ,"-", referenceSW[idsSW[i]] ," = ", referenceHW[idsHW[i]]-referenceSW[idsSW[i]]
-
-	poses = []
-	Leg.references = (148.38709677419354, 161.87683284457478, 88.26979472140764)
-
-	peter = Spidey(ctrl)
-
+	# Leg.references = (148.38709677419354, 161.87683284457478, 88.26979472140764)
 	# peter.led = True
+	
+##########################################################################################
 
-	while(raw_input("Press ENTER when the pose is ready ...") != 's'):
-		peter.compliant = True
-		raw_input("Confirm POSE NOW !")
-		peter.compliant = False
-		poses.append(peter.raw_pose())
-		peter.compliant = True
+	# ctrl = initCtrl()
+	# peter = Spidey(ctrl)
+	# if len(sys.argv) == 2:
+	# 	poseFilename = sys.argv[1]
+	# 	poseFile = open(poseFilename, 'r')
+	# 	poses = json.loads(poseFile.read())
+	# 	raw_input("Press ENTER to play the moves")
+	# 	peter.set_pose_move(poses)
+	# 	while peter.current_pose_index < len(poses)-1:
+	# 		peter.play_pose_move()
+	# 		ctrl.wait(10)
+	# 	peter.play_pose_move()
+	# 	ctrl.wait(10)
+	# else:
+	# 	poseFilename = "tmp.move"
+	# 	poseFile = open(poseFilename, 'w')
+	# 	poses = []
+	# 	while(raw_input("Press ENTER to continue or S to stop ...") != 's'):
+	# 		peter.compliant = True
+	# 		raw_input("Press ENTER to confirm the pose ...")
+	# 		peter.compliant = False
+	# 		poses.append(peter.raw_pose())
+	# 	poseFile.write(json.dumps(poses))
+	# 	poseFile.close()
+	# peter.compliant = True
+	# ctrl.wait(1)
 
-	# myFile = open('walk.move', 'w')
-	# myFile.write(poses)
-	# myFile.close()
+##########################################################################################
 
-	print(poses)
+	# peter.compliant = False
+	# raw_input("ENTER to play the move for a long time")
+	# peter.set_pose_move(poses)
 
-	peter.compliant = False
-	raw_input("ENTER to play the move for a long time")
-	peter.set_pose_move(poses)
+	# while True:
+	# 	peter.play_pose_move()
+	# 	ctrl.wait(5)
 
-	while True:
-		peter.play_pose_move()
-		ctrl.wait(20)
-
-	ctrl.wait(5)
+	# ctrl.wait(5)
 
 
 def initCtrl():
-	if len(sys.argv) == 2:
-		min_id, max_id = int(sys.argv[1]), int(sys.argv[1])
-	else:
-		min_id, max_id = 0, 20
-
-	ctrl = dyn.create_controller(verbose = True, motor_range = [min_id, max_id])
+	min_id, max_id = 0, 20
+	ctrl = dyn.create_controller(verbose=True, motor_range=[min_id, max_id], timeout=0.05)
+	for motor in ctrl.motors:
+		motor.max_torque = 50
 	sys.stdout.flush()
 	return ctrl
 
@@ -117,6 +127,18 @@ def sinAnimationMode(ctrl):
 		for m in ctrl.motors:
 			m.led = True
 			m.position = 80 + math.sin(elapsedTime) * 30
+
+def drawSquareMode(ctrl):
+	peter = Spidey(ctrl)
+	setMotorsCompliant(ctrl, False)
+	positions = [Vector3D(0,3,3),Vector3D(0,3,6),Vector3D(0,6,6),Vector3D(0,6,3)]
+	frame = 0
+	while 1 :
+		print "a"
+		peter.legs[0].move(positions[frame%4])
+		frame += 1
+		ctrl.wait(10)
+
 
 if __name__ == "__main__":
 	main()
